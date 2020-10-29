9fb22ba22b1dccbb77a76bf929f6c2102cf957ee
MorganaLF <4743866@mail.ru>
2020-10-29 11:06:01 +0300

    fix: fix types 'contents' import

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ import fromWeb3DataEvent from './ethereum/fromWeb3DataEvent';
 import getContractData$ from './ethereum/getContractData$';
 // @ts-ignore
 import makeContractCreator from './ethereum/makeContractCreator';
+// @ts-ignore
+import types from './ethereum/types';
 import { transform } from './transform';
 
 export interface IWeb3Cfg {
@@ -78,7 +80,7 @@ export default class Web3 extends TsGeneratorPlugin {
       },
       {
         path: join(this.outDirAbs, 'utils/types.ts'),
-        contents: makeContractCreator,
+        contents: types,
       },
       {
         path: join(this.outDirAbs, 'index.ts'),
